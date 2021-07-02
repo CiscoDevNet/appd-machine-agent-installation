0f9321f15e328503a1c7d18bd37eb578d588ad5d
Mel Delgado <delgadm@cisco.com>
2021-07-02 13:13:35 -0700

    Redo TF provisioner approach

--- da-compute-apache-web-server.tf
+++ da-compute-apache-web-server.tf
@@ -107,28 +107,6 @@ resource "vsphere_virtual_machine" "vm1" {
     thin_provisioned = "${data.vsphere_virtual_machine.template.disks.0.thin_provisioned}"
   }
 
-  provisioner "remote-exec"  {
-    inline = [
-    "mkdir /home/delgadm/.ssh",
-    "chmod 700 /home/delgadm/.ssh",
-    "touch /home/delgadm/.ssh/authorized_keys",
-    "chmod 600 /home/delgadm/.ssh/authorized_keys",
-    "echo ${var.ssh-pub-key} >> /home/delgadm/.ssh/authorized_keys"
-    ]
-
-    connection {
-    type     = "ssh"
-    user     = "${var.service_account_username}"
-    password = "${var.service_account_password}"
-    host     = "10.200.0.${101 + count.index}"
-  }
-
-  }
-  
-  provisioner "local-exec" {
-   command = "ansible-playbook -u delgadm -i apache-web-servers.txt main.yml --vault-password-file ./.vault_pass.txt"
-   }
-
   clone {
     template_uuid = "${data.vsphere_virtual_machine.template.id}"
 
@@ -148,4 +126,30 @@ resource "vsphere_virtual_machine" "vm1" {
     }
   }
 
+  provisioner "remote-exec"  {
+    inline = [
+    "mkdir /home/delgadm/.ssh",
+    "chmod 700 /home/delgadm/.ssh",
+    "touch /home/delgadm/.ssh/authorized_keys",
+    "chmod 600 /home/delgadm/.ssh/authorized_keys",
+    "echo ${var.ssh-pub-key} >> /home/delgadm/.ssh/authorized_keys"
+    ]
+
+    connection {
+    type     = "ssh"
+    user     = "${var.service_account_username}"
+    password = "${var.service_account_password}"
+    host     = "10.200.0.${101 + count.index}"
+    }
+  }
+  
+} # "vsphere_virtual_machine" "vm1"
+
+resource "null_resource" "ansible-playbook" {
+  # Call Ansible from our local host where Terraform runs but only after the machines are created
+  provisioner "local-exec" {
+   command = "ansible-playbook -u delgadm -i apache-web-servers.txt main.yml --vault-password-file ./.vault_pass.txt"
+   }
+
+   depends_on = ["vsphere_virtual_machine.vm1"] # Let's not kick off this resource until the VMs are created
 }
